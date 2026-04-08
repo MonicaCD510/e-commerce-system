787a024e9046819a98f64a40f8f8a377cbc318cc
Monica Davila <monica.davila510@gmail.com>
2026-04-08 18:36:59 -0500

    formatted product output with template strings

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { getProducts } from "./services/apiService";
 async function run() {
   const products = await getProducts();
 
-// console.log(products[0]);
-// console.log(products[0].title);
-// console.log(products[0].price);
+  // OLD CODE (kept for reference)
+  // console.log(products[0]);
+  // console.log(products[0].title);
+  // console.log(products[0].price);
 
-products.forEach((product: { title: any; price: any }) => {
-  console.log(product.title, product.price);
-});
+  
+  products.forEach((product: any) => {
+    console.log(`Product: ${product.title} - Price: $${product.price}`);
+  });
+}
 
-}run();
+run();
