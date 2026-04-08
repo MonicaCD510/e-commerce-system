3173f0693ddd96cc13cfaa94483e8141af13770a
Monica Davila <monica.davila510@gmail.com>
2026-04-08 18:08:23 -0500

    displayed first product title and price

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { getProducts } from "./services/apiService";
 
 async function run() {
   const products = await getProducts();
-  console.log(products);
-}
 
-run();
+console.log(products[0]);
+console.log(products[0].title);
+console.log(products[0].price);
+
+}run();
