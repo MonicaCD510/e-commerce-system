08b7d6f57d41e667e626053e0c6596b5db265c62
Monica Davila <monica.davila510@gmail.com>
2026-04-08 18:23:15 -0500

    looped through products and displayed all titles

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { getProducts } from "./services/apiService";
 async function run() {
   const products = await getProducts();
 
-console.log(products[0]);
-console.log(products[0].title);
-console.log(products[0].price);
+// console.log(products[0]);
+// console.log(products[0].title);
+// console.log(products[0].price);
+
+products.forEach ((product: { title: any; }) => {
+  console.log(product.title);
+});
 
 }run();
