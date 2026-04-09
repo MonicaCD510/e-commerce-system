6802e48276fb0a1b456886dac8a67c27cb8e44fe
Monica Davila <monica.davila510@gmail.com>
2026-04-08 20:05:23 -0500

    implemented custom error handling in api service using CustomError class

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,9 +1,13 @@
+import { CustomError } from "../utils/errorHandler";
+
+
+
 export async function getProducts() {
   try {
     const response = await fetch("https://dummyjson.com/products");
 
     if (!response.ok) {
-      throw new Error("Failed to fetch products");
+      throw new CustomError("Failed to fetch products");
     }
 
     const data = await response.json();
