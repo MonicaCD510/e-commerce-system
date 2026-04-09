70ec53a1d8acc66897142808ab4a037308d61142
Monica Davila <monica.davila510@gmail.com>
2026-04-08 19:56:51 -0500

    integrated product class with discount and tax utilities in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { getProducts } from "./services/apiService";
 import { Product } from "./models/product";
+import { calculateDiscount } from "./utils/discountCalculator";
+import { calculateTax } from "./utils/taxCalculator";
 
 async function run() {
   const products = await getProducts();
+   const productObjects = products.map((product: any) => {
+    return new Product(product.title, product.price);
+  });
+
+  productObjects.forEach((product: Product) => {
+    product.displayDetails();
+
+    const discountAmount = calculateDiscount(product.price, 10);
+    console.log("Discount Amount:", discountAmount);
+
+    const taxAmount = calculateTax(product.price, "beauty");
+    console.log("Tax Amount:", taxAmount);
+  });
+}
 
   // OLD CODE (kept for reference)
   // console.log(products[0]);
@@ -27,16 +43,16 @@ async function run() {
 
 // console.log("Total products over $500:", count);
 
-const expensiveProducts = products.filter((product: any) => {
-  return product.price > 500;
-});
+// const expensiveProducts = Product.filter((product: any) => {
+//   return product.price > 500;
+// });
 
-expensiveProducts.forEach((product: any) => {
-  console.log(`Product: ${product.title} - Price: $${product.price}`);
-});
+// expensiveProducts.forEach((product: any) => {
+//   console.log(`Product: ${product.title} - Price: $${product.price}`);
+// });
 
-console.log("Total:", expensiveProducts.length);
+// console.log("Total:", expensiveProducts.length);
 
-}
+
 
 run();
